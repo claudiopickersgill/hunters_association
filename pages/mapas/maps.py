6f5cff8d3feab94d806bfe2f016dd53c418c3b59
Claudio Pickersgill Filho <127744294+claudiopickersgill@users.noreply.github.com>
2024-06-22 00:11:25 -0300

    almost there

--- pages/mapas/maps.py
+++ pages/mapas/maps.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
-from streamlit_folium import st_folium
+from streamlit_folium import st_folium, folium_static
 import folium
 from shapely.geometry import Point, Polygon
+import json
+import utils_config
+import random
 
 
 def maps():
@@ -10,6 +13,9 @@ def maps():
     st.write("Acompanhe aqui em tempo real os portais que estão ativos no país!")
     # st.write("Em breve!")
     st.divider()
+
+    config_file = "./config.json"
+    config = utils_config.load_config(config_file)
 
     # Defina a localização inicial do mapa (latitude e longitude)
     initial_location = [39.976177, -97.728974]
@@ -27,15 +33,57 @@ def maps():
         folium.Polygon(locations=[(lat, lon) for lat, lon in polygon.exterior.coords],
                        color=color, fill=True, fill_opacity=0.4).add_to(m)
 
-    # # Função para adicionar marcadores ao mapa
-    # def add_markers_to_map(m, points):
-    #     for lat, lon in points:
-    #         folium.Marker(location=[lat, lon],
-    #                       icon=folium.Icon(color='red')).add_to(m)
+    def add_markers_to_map(m, lat, long, rank, dias, prioridade):
+        if prioridade == "alta":
+            html = f"""Rank: {rank}<br>
+                       Dias Abertos: {dias}<br>
+                       Prioridade: {prioridade}"""
+            iframe = folium.IFrame(html=html, width=150, height=80)
+            popup = folium.Popup(iframe, max_width=300)
+            folium.Marker(location=[long, lat],
+                          popup=popup,
+                          icon=folium.Icon(color='red')).add_to(m)
+        elif prioridade == "media":
+            html = f"""Rank: {rank}<br>
+                       Dias Abertos: {dias}<br>
+                       Prioridade: {prioridade}"""
+            iframe = folium.IFrame(html=html, width=150, height=80)
+            popup = folium.Popup(iframe, max_width=300)
+            folium.Marker(location=[long, lat],
+                          popup=popup,
+                          icon=folium.Icon(color='darkgreen')).add_to(m)
+        else:
+            html = f"""Rank: {rank}<br>
+                       Dias Abertos: {dias}<br>
+                       Prioridade: {prioridade}"""
+            iframe = folium.IFrame(html=html, width=150, height=80)
+            popup = folium.Popup(iframe, max_width=300)
+            folium.Marker(location=[long, lat],
+                          popup=popup,
+                          icon=folium.Icon(color='lightgreen')).add_to(m)
 
-    def add_markers_to_map(m, lat, long):
-        folium.Marker(location=[lat, long],
-                      icon=folium.Icon(color='red')).add_to(m)
+    def add_locals_to_map(m, config):
+        for i in config:
+            if i.name == "Guilda Sombras":
+                pass
+            elif i.name == "Quebra de Portal":
+                html = f"""Nome: {i.name}<br>
+                          Rank: {i.rank}<br>
+                          Sede: {i.sede}"""
+                iframe = folium.IFrame(html=html, width=300, height=80)
+                popup = folium.Popup(iframe, max_width=300)
+                folium.Marker(location=[i.lat, i.long],
+                              popup=popup,
+                              icon=folium.Icon(color="black")).add_to(m)
+            else:
+                html = f"""Nome: {i.name}<br>
+                          Rank: {i.rank}<br>
+                          Sede: {i.sede}"""
+                iframe = folium.IFrame(html=html, width=300, height=80)
+                popup = folium.Popup(iframe, max_width=300)
+                folium.Marker(location=[i.lat, i.long],
+                              popup=popup,
+                              icon=folium.Icon(color="blue")).add_to(m)
 
     # # Função para verificar se um ponto está dentro do polígono
     # def check_location_in_us(location):
@@ -46,76 +94,48 @@ def maps():
     m = folium.Map(location=initial_location, zoom_start=4)
 
     # Lendo os arquivos CSV e criando os polígonos
-    file_paths = ['data/csv/ALASKA_CSV.csv',
-                  'data/csv/BASE_CUBA_CSV.csv',
-                  'data/csv/EUA_PONTOS_CSV.csv',
-                  'data/csv/ILHA_OCEANO_CSV.csv']
+    # file_paths = ['data/csv/ALASKA_CSV.csv',
+    #                 'data/csv/EUA_PONTOS_CSV.csv']
+    # colors = ['blue', 'green', 'red', 'purple']
+    # polygons = []
+    # all_portals = []
 
-    colors = ['blue', 'green', 'red', 'purple']
-    polygons = []
-    all_portals = []
+    # portals = read_csv_to_points('data/csv/portals.csv')
+    portals_df = pd.DataFrame(pd.read_csv('data/csv/portals.csv'))
 
-    # portals_paths = ['data/csv/eua_portals.csv',
-    #                  'data/csv/alaska_portals.csv',
-    #                  'data/csv/cuba_portals.csv']
+    # for file_path in file_paths:
+    #     points = read_csv_to_points(file_path)
+    #     polygon = create_polygon(points)
+    #     polygons.append(polygon)
 
-    portals = read_csv_to_points('data/csv/eua_portals.csv')
+    us_points = read_csv_to_points('data/csv/EUA_PONTOS_CSV.csv')
+    alaska_points = read_csv_to_points('data/csv/ALASKA_CSV.csv')
+    us_polygon = create_polygon(us_points)
+    alaska_polygon = create_polygon(alaska_points)
 
-    # for portal_path in portals_paths:
-    #     points = read_csv_to_points(portal_path)
-    #     all_portals.append(points)
+    # for i, polygon in enumerate(polygons):
+    # add_polygon_to_map(m, us_polygon, color="blue")
+    # add_polygon_to_map(m, alaska_polygon, color="green")
 
-    for file_path in file_paths:
-        points = read_csv_to_points(file_path)
-        polygon = create_polygon(points)
-        polygons.append(polygon)
-        # all_portals.append(portals)
+    # def check_location_in_us(lat, lon):
+    #     point = Point(lon, lat)  # Shapely usa (lon, lat)
+    #     return polygon.contains(point)
 
-    for i, polygon in enumerate(polygons):
-        # add_polygon_to_map(m, polygon, color=colors[i])
+    X = 250  # Altere este valor conforme necessário
 
-        def check_location_in_us(lat, lon):
-            point = Point(lon, lat)  # Shapely usa (lon, lat)
-            return polygon.contains(point)
+    # Selecionar aleatoriamente X linhas do DataFrame
+    random_indices = random.sample(range(len(portals_df)), X)
+    random_rows = portals_df.iloc[random_indices]
 
-        for lat, long in portals:
-            if check_location_in_us(long, lat):
-                add_markers_to_map(m, lat, long)
-            else:
-                pass
+    def plot_markers():
+        for index, row in random_rows.iterrows():
+            point = Point(row['y'], row['x'])
+            if (us_polygon.contains(point) == True) or (alaska_polygon.contains(point) == True):
+                add_markers_to_map(
+                    m, row["x"], row["y"], row["rank"], row["dias_aberto"], row["prioridade"])
 
-    # # Cria um polígono usando Shapely
-    # us_polygon = Polygon(us_polygon_coords)
-
-    # locations = [
-    #     {"name": "São Francisco", "coords": (-122.4194, 37.7749)},
-    #     {"name": "Los Angeles", "coords": (-118.2437, 34.0522)},
-    #     {"name": "Nova York", "coords": (-74.0060, 40.7128)},
-    #     {"name": "Chicago", "coords": (-87.6298, 41.8781)},
-    #     {"name": "Toronto", "coords": (-79.3832, 43.6532)}  # Fora dos EUA
-    # ]
-
-    # Adicionar o polígono dos EUA ao mapa
-    # folium.Polygon(locations=us_polygon_coords, color="grey",
-    #                fill_opacity=0.1,
-    #                weight=1,
-    #                fill=False).add_to(m)
-
-    # for location in locations:
-    #     folium.Marker(
-    #         # Invertendo para folium (lat, lon)
-    #         location=location["coords"][::-1],
-    #         popup=f"{location['name']} - Dentro dos EUA: {check_location_in_us(location)}",
-    #         icon=folium.Icon(
-    #             color="green" if check_location_in_us(location) else "red")
-    #     ).add_to(m)
-
-    # # Adicione um marcador ao mapa
-    # folium.Marker(
-    #     location=initial_location,
-    #     popup="São Francisco",
-    #     icon=folium.Icon(icon="cloud"),
-    # ).add_to(m)
+    plot_markers()
+    add_locals_to_map(m, config)
 
     # Adicione a chave de API do Google Maps ao Folium (opcional)
     # Você pode adicionar camadas do Google Maps (se necessário)
@@ -132,6 +152,6 @@ def maps():
 
     # Use st_folium para renderizar o mapa no Streamlit
     # st.title("Mapa Interativo com Google Maps e Streamlit")
-    st_folium(m, width=700, height=500)
+    folium_static(m, width=700, height=500)
 
     return
